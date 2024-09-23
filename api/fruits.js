185740c062da560b371839913a5668df74fc40f8
Angad Pal <Angad4548p@gmail.com>
2024-09-23 22:49:53 +0530

    fruits.js

--- api/fruits.js
+++ api/fruits.js
@@ -7,112 +7,112 @@ const fruits = [
     {
         id: 1, 
         name: 'Apple', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/apple.jpeg'
     },
     {
         id: 2, 
         name: 'Banana', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/bananas.jpeg'
     },
     {
         id: 3, 
         name: 'Orange', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/orange.jpeg'
     },
     {
         id: 4, 
         name: 'Date', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/date.jpeg'
     },
     {
         id: 5, 
         name: 'Grape', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/grape.jpeg'
     },
     {
         id: 6, 
         name: 'Strawberry', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/strawberry.jpeg'
     },
     {
         id: 7, 
         name: 'Watermelon', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/watermelon.jpeg'
     },
     {
         id: 8, 
         name: 'Pineapple', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/pineapple.jpeg'
     },
     {
         id: 9, 
         name: 'Mango', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/mango.jpeg'
     },
     {
         id: 10, 
         name: 'Peach',
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/peach.jpeg'
     },
     {
         id: 11, 
         name: 'Cherry', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/cherry.jpeg'
     },
     {
         id: 12, 
         name: 'Blueberry', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/blueberry.jpeg'
     },
     {
         id: 13, 
         name: 'Kiwi', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/kiwi.jpeg'
     },
     {
         id: 14, 
         name: 'Pear', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/pear.jpeg'
     },
     {
         id: 15, 
         name: 'Plum', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/plum.jpeg'
     },
     {
         id: 16, 
         name: 'Papaya', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/papaya.jpeg'
     },
     {
         id: 17, 
         name: 'Guava', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/guava.jpeg'
     },
     {
         id: 18, 
         name: 'Fig', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/fig.jpeg'
     },
     {
         id: 19, 
         name: 'Coconut', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/coconut.jpeg'
     },
     {
         id: 20, 
         name: 'Pomegranate', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/pomegranate.jpeg'
     },
     {
         id: 21, 
         name: 'Raspberry', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/raspberry.jpeg'
     },
     {
         id: 22, 
         name: 'Dragonfruit', 
-        image: '#'
+        image: 'http://vocabulary-api-codewithangad.vercel.app/fruits/dragonfruit.jpeg'
     },
 ]
 
